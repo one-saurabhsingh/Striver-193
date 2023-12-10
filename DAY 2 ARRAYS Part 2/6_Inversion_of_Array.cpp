f2c87e01822ef5f2af140003ca4fccc99c3b80e1
MARCOS <saurabh887799@gmail.com>
2023-12-11 00:26:35 +0530

    Completed

---
+++ DAY 2 ARRAYS Part 2/6_Inversion_of_Array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+long long getInversions(long long *a, int n)
+{
+    // BRUTE FORCE
+    // int count = 0;
+    // for(int i=0; i< n; i++){
+    //     for(int j = i + 1; j <n ; j++){
+    //         if(a[i] > a[j]){
+    //             count+=1;
+    //         }
+    //     }
+    // }
+    // return count;
+
+    // <---------OPTIMAL APPROACH (MERGE SORT) -------------------->
+    // DO IT YOURSELF.
+}
